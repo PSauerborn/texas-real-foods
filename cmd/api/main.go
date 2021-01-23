5eb97cc54bcf0110396d6df0fa8452b9ce1384f3
pascal sauerborn <pascal.sauerborn@gmail.com>
2021-01-23 11:01:09 +0000

    further formatting changes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	log "github.com/sirupsen/logrus"
+    log "github.com/sirupsen/logrus"
 
-	"texas_real_foods/pkg/api"
+    "texas_real_foods/pkg/api"
 )
 
 func main() {
-	log.SetLevel(log.DebugLevel)
+    log.SetLevel(log.DebugLevel)
 
-	router := api.New()
-	router.Run(":10999")
+    router := api.New()
+    router.Run(":10999")
 }
