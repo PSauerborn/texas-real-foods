51a485819b9457d5714b75926fa3c0140a2f2feb
pascal sauerborn <pascal.sauerborn@gmail.com>
2021-01-29 09:51:27 +0000

    updated formatting

--- go/pkg/authenticator/persistence.go
+++ go/pkg/authenticator/persistence.go
@@ -1,11 +1,11 @@
 package authenticator
 
 import (
-	"fmt"
-	"context"
+    "fmt"
+    "context"
 
-	"github.com/jackc/pgx/v4"
-	log "github.com/sirupsen/logrus"
+    "github.com/jackc/pgx/v4"
+    log "github.com/sirupsen/logrus"
 )
 
 type Persistence struct{
@@ -36,46 +36,46 @@ func(db *Persistence) Connect() (*pgx.Conn, error) {
 
 // function to retrieve all API access keys from database
 func(db *Persistence) GetAPIKeys() ([]string, error) {
-	log.Debug("retrieving API keys")
+    log.Debug("retrieving API keys")
 
-	results := []string{}
+    results := []string{}
 
-	query := `SELECT key FROM access_keys`
-	rows, err := db.Session.Query(context.Background(), query)
-	if err != nil {
-		switch err {
-		case pgx.ErrNoRows:
-			return results, nil
-		default:
-			return results, err
-		}
-	}
+    query := `SELECT key FROM access_keys`
+    rows, err := db.Session.Query(context.Background(), query)
+    if err != nil {
+        switch err {
+        case pgx.ErrNoRows:
+            return results, nil
+        default:
+            return results, err
+        }
+    }
 
-	for rows.Next() {
-		var key string
-		if err := rows.Scan(&key); err != nil {
-			log.Warn(fmt.Errorf("unable to read key into local variable: %+v", err))
-			continue
-		}
-		results = append(results, key)
-	}
-	return results, nil
+    for rows.Next() {
+        var key string
+        if err := rows.Scan(&key); err != nil {
+            log.Warn(fmt.Errorf("unable to read key into local variable: %+v", err))
+            continue
+        }
+        results = append(results, key)
+    }
+    return results, nil
 }
 
 // function used to check if a given API key is valid
 func(db *Persistence) IsValidApiKey(key string) (bool, error) {
-	log.Debug(fmt.Sprintf("validating API key %s", key))
+    log.Debug(fmt.Sprintf("validating API key %s", key))
 
-	var result string
-	query := `SELECT key FROM access_keys WHERE key=$1`
-	row := db.Session.QueryRow(context.Background(), query, key)
-	if err := row.Scan(&result); err != nil {
-		switch err {
-		case pgx.ErrNoRows:
-			return false, nil
-		default:
-			return false, err
-		}
-	}
-	return true, nil
+    var result string
+    query := `SELECT key FROM access_keys WHERE key=$1`
+    row := db.Session.QueryRow(context.Background(), query, key)
+    if err := row.Scan(&result); err != nil {
+        switch err {
+        case pgx.ErrNoRows:
+            return false, nil
+        default:
+            return false, err
+        }
+    }
+    return true, nil
 }
