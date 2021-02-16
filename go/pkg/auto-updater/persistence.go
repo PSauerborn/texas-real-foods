c265dd0ce1c06427d5cc25de819c32d94932364e
pascal sauerborn <pascal.sauerborn@gmail.com>
2021-02-16 17:53:52 +0000

    added additional query for insertion of timeseries data values

--- go/pkg/auto-updater/persistence.go
+++ go/pkg/auto-updater/persistence.go
@@ -28,18 +28,28 @@ func NewPersistence(url string) *Persistence {
 // updates are done as inserts i.e. existing values are overwritten
 func(db *Persistence) UpdateBusinessData(update connectors.BusinessUpdate) error {
     log.Debug(fmt.Sprintf("updating business %+v", update))
-    var query string
 
+    var (query string; err error)
     // execute query to insert new data arguments
     query = `INSERT INTO asset_data(business_id,phone,website_live,source,open)
         VALUES($1,$2,$3,$4,$5) ON CONFLICT (business_id,source) DO UPDATE
         SET phone=$2, website_live=$3, open=$5`
-    _, err := db.Session.Exec(context.Background(), query, update.Meta.BusinessId,
+    _, err = db.Session.Exec(context.Background(), query, update.Meta.BusinessId,
         update.Data.BusinessPhones, update.Data.WebsiteLive, update.Data.Source, update.Data.BusinessOpen)
     if err != nil {
+        log.Error(fmt.Errorf("unable to insert data into static table: %+v", err))
         return err
     }
 
+    // add entry into time series table
+    query = `INSERT INTO asset_data_timeseries(business_id,source,phone,website_live,open)
+        VALUES($1,$2,$3,$4,$5)`
+    _, err = db.Session.Exec(context.Background(), query, update.Meta.BusinessId,
+        update.Data.Source, update.Data.BusinessPhones, update.Data.WebsiteLive, update.Data.BusinessOpen)
+    if err != nil {
+        log.Error(fmt.Errorf("unable to insert data into timeseries table: %+v", err))
+        return err
+    }
     // update metadata with last update flag
     query = `UPDATE asset_metadata SET last_update=$1 WHERE business_id=$2`
     _, err = db.Session.Exec(context.Background(), query, time.Now(), update.Meta.BusinessId)
